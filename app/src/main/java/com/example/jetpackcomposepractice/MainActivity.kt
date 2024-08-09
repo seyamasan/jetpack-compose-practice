a5e024569da11750f1a720ffff0404854b4c5dd6
seyamasan <kst_apr_24@icloud.com>
2024-08-09 14:07:52 +0900

    7章終了

--- app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -6,17 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEachIndexed
+
+/*
+* Jetpack Composeの勉強をするためだけのActivity
+*/
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,21 +48,27 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, index: Int, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) } // ボタンの可変状態を記憶
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello ")
                 Text(text = name)
             }
             ElevatedButton(
                 onClick = {
-                    println("OK: $index")
+                    println("Index is: $index")
+                    expanded.value = !expanded.value
                 }
             ) {
-                Text("Show more $index")
+                Text(if (expanded.value) "Show less $index" else "Show more $index")
             }
         }
     }
