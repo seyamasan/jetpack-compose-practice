2b0d520a42e379e134ac4ffa80997dbc957c2d34
seyamasan <kst_apr_24@icloud.com>
2024-08-09 16:24:23 +0900

    8章終了

--- app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -3,17 +3,22 @@ package com.example.jetpackcomposepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,7 +40,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            // 初回のみ表示
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Jetpack Compose!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
@@ -48,14 +88,15 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, index: Int, modifier: Modifier = Modifier) {
-    val expanded = remember { mutableStateOf(false) } // ボタンの可変状態を記憶
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by remember { mutableStateOf(false) } // ボタンの可変状態を記憶
+    val extraPadding = if (expanded) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
+            Column(
+                modifier = Modifier
                 .weight(1f)
                 .padding(bottom = extraPadding)
             ) {
@@ -65,12 +106,20 @@ fun Greeting(name: String, index: Int, modifier: Modifier = Modifier) {
             ElevatedButton(
                 onClick = {
                     println("Index is: $index")
-                    expanded.value = !expanded.value
+                    expanded = !expanded
                 }
             ) {
-                Text(if (expanded.value) "Show less $index" else "Show more $index")
+                Text(if (expanded) "Show less $index" else "Show more $index")
             }
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    MaterialTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
@@ -78,6 +127,14 @@ fun Greeting(name: String, index: Int, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MaterialTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    MaterialTheme {
+        MyApp(Modifier.fillMaxSize())
     }
 }
