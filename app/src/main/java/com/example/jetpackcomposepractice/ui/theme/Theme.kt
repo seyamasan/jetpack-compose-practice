3c5b13b3bac6fbc1704a7ab9829ddade0b8f8ffd
seyamasan <kst_apr_24@icloud.com>
2024-08-10 22:37:55 +0900

    いらないインポート消した

--- app/src/main/java/com/example/jetpackcomposepractice/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/ui/theme/Theme.kt
@@ -5,7 +5,6 @@ import android.os.Build
 import android.view.View
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
